82553a35d2e7c9374e2517185d70588a0386b449
Markus Dürauer <markus.duerauer@feuerwehr.gv.at>
2017-01-03 17:48:34 +0100

    Added server

---
+++ src/bin/server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "wab.h"
+#include "server.h"
+
+int main(int argc, char* argv[]) {
+	int rv = 0;
+	printf("Hello Server!\n");
+	return rv;
+}
---
+++ src/bin/wab.h
@@ -0,0 +1,1 @@
+#define WAB	123
